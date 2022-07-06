0aa63d64f03942c57cd13a3e074f9356d07fb6c6
Apollo Arantes Campos <a.arantes.campos@avanade.com>
2022-07-06 13:50:55 -0300

    https://github.com/ApolloArantes/alura-studies.git

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 function List() {
+    const tarefas = [{
+        tasks: 'React',
+        time: '02:00:00'
+    }, {
+        tasks: 'Javascript',
+        time: '01:00:00'
+    }, {
+        tasks: 'Typescript',
+        time: '03:00:00'
+    }]
     return (
         <aside>
             <h2> Estudos do dia </h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Javascript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tarefas.map((item, index) => (
+                    <li key={index}>
+                        <h3> {item.tasks} </h3>
+                        <span> {item.time} </span>
+                    </li>
+                ))}                
             </ul>
         </aside>
     )
